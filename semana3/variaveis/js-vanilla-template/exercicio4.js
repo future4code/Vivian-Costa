ac95d9b960e1e3b76128d040a184904f482a6792
Vivian-Oliveira <costa.vivs@gmail.com>
2020-11-30 17:34:28 +0000

    exercicios de JS

---
+++ semana3/variaveis/js-vanilla-template/exercicio4.js
@@ -0,0 +1,6 @@
+let perguntasBoole = ["Esta chovendo hoje? ", "Voce gosta de viajar? ", "Voce gosta de estudar? "]
+let respostas = [false, true, true]
+
+console.log(perguntasBoole[0], respostas[0]);
+console.log(perguntasBoole[1], respostas[1]);
+console.log(perguntasBoole[2], respostas[2]);
