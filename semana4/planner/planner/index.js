29b714ebb677d86a1d203f46ff95f8db8028e576
Vivian-Oliveira <costa.vivs@gmail.com>
2020-12-08 20:01:20 +0000

    Exercicio DOM

---
+++ semana4/planner/planner/index.js
@@ -0,0 +1,10 @@
+function addTask(){
+    const input = document.getElementById('tarefa')
+    const inputValue = input.value
+
+    const select = document.getElementById('dias-semana')
+    const selectValue = select.value
+
+    const div = document.getElementById(selectValue)
+div.innerHTML += `<p>${inputValue}</p>`
+}
