30541bdff3b827475e73a2e289c5bbce4d704973
Vivian-Oliveira <costa.vivs@gmail.com>
2020-12-11 17:41:28 +0000

    Exercicio 2 - escrita de codigo

--- semana4/lista-de-exercicios/ex-escrita-cod.js
+++ semana4/lista-de-exercicios/ex-escrita-cod.js
@@ -18,4 +18,25 @@
 // console.log(exemplo3)
 // console.log(exemplo3.length)
 
+//Exercicio 2- Para este exercício considere as seguintes variáveis:
+
+// const booleano1 = true
+// const booleano2 = false
+// const booleano3 = !booleano2 //true
+// const booleano4 = !booleano3 //false
+
+//a) booleano1 && booleano2 && !booleano4
+//Resposta: false
+
+//b) (booleano1 && booleano2) || !booleano3
+//Resposta: true
+
+//c)  (booleano2 || booleano3) && (booleano4 || booleano1)
+//Resposta: true
+
+//d) !(booleano2 && booleano3) || !(booleano1 && booleano3)
+//Resposta: true
+
+//e) !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+//Resposta: false
 
