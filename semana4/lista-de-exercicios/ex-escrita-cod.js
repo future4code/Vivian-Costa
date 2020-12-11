0d1b2ea8d72ffc8512e1fff2f0a5d230adc3d5df
Vivian-Oliveira <costa.vivs@gmail.com>
2020-12-11 17:24:46 +0000

    Exercicio 1 - escrita de codigo

---
+++ semana4/lista-de-exercicios/ex-escrita-cod.js
@@ -0,0 +1,21 @@
+//Exercicio 1 - Cite 3 maneiras de se percorrer/iterar uma lista. Faça um programa para exemplificar.
+
+// const exemplo1 = ["banana", "pera", "laranja", "mexerica"]
+
+// for(let frutas of exemplo1){
+//     console.log(frutas)
+//   }
+
+// const exemplo2 = ["guitarra", "baixo", "bateria", "ukulele"]
+
+// for(let i = 0; i < 4; i++){
+//     const instrumentos = exemplo2[i]
+//     console.log(instrumentos)
+// }
+
+// const exemplo3 = ["bola", "batata", "bateria", "ukulele"]
+
+// console.log(exemplo3)
+// console.log(exemplo3.length)
+
+
